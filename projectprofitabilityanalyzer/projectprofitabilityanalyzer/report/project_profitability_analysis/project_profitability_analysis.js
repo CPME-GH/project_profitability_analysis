26fcfa3bb10d29787340d43cdc3d3e6c8e0a95e7
Pranav-P-GIT <pranavpppp1@gmail.com>
2024-02-01 12:42:57 +0530

    feat:Project Profitability Analysis Report

---
+++ projectprofitabilityanalyzer/projectprofitabilityanalyzer/report/project_profitability_analysis/project_profitability_analysis.js
@@ -0,0 +1,48 @@
+// Copyright (c) 2024, Craft and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+frappe.query_reports["Project Profitability Analysis"] = {
+    "filters": [
+        {
+            'fieldname': 'project',
+            'label': __("Project"),
+            'fieldtype': 'Link',
+            'options': 'Project'
+        }
+    ],
+    "formatter": function (value, row, column, data, default_formatter) {
+        console.log(data)
+        console.log(data.currency)
+        if (column.fieldname === "voucher_no") {
+            value = frappe.format(value, {fieldtype: 'Link', options: data.voucher_type});
+        } else if (column.fieldname === "qty") {
+            if (value === '') {
+                value = '';
+            } else {
+                value = parseFloat(value).toFixed(2);
+            }
+        } else if (column.fieldname === "rate") {
+            if (value === '' || value === 0) {
+                value = '';      
+            } else {
+                value = parseFloat(value).toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2}) + " " + data.currency;
+            }
+        } else if (column.fieldname === "amount") {
+            if (value === ' ' || value === '' || value === 0) {
+                value = '';
+            } else if (data.is_percentage) {
+                value = parseFloat(value).toFixed(2) + "%";
+            } else {
+                value = parseFloat(value).toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2}) + " " + data.currency;
+            }
+        } else {
+            value = default_formatter(value, row, column, data);
+        }
+        if (data && data.indent == 0) {
+            value = $(`<span>${value}</span>`);
+            var $value = $(value).css("font-weight", "bold");
+            value = $value.wrap("<p></p>").parent().html();
+        }
+        return value;
+    }
+};
---
+++ projectprofitabilityanalyzer/projectprofitabilityanalyzer/report/project_profitability_analysis/project_profitability_analysis.py
@@ -0,0 +1,301 @@
+import frappe
+
+def execute(filters=None):
+    if filters is None:
+        filters = {}
+        
+    columns = get_columns()
+    data = get_data(filters)
+    return columns, data
+
+def get_columns():
+    return [
+        {"label": "Item", "fieldname": "item", "fieldtype": "Data", "width": 300},
+        {"label": "Voucher Type", "fieldname": "voucher_type", "fieldtype": "Data", "width": 200, "align":"center"},
+        {"label": "Voucher No", "fieldname": "voucher_no", "fieldtype": "Data", "width": 200, "align":"center"},
+        {"label": "Qty", "fieldname": "qty", "fieldtype": "Data", "width": 100, "align":"center"},
+        {"label": "Rate", "fieldname": "rate", "fieldtype": "Currency", "width": 200},
+        {"label": "Amount", "fieldname": "amount", "fieldtype": "Currency", "width": 200}
+    ]
+
+def get_data(filters):
+    if 'project' not in filters:
+        return []
+    
+    company = frappe.db.get_value("Project", filters['project'], "company")
+    currency = frappe.db.get_value("Company", company, "default_currency")
+
+
+    sales_orders = frappe.db.sql("""
+        SELECT
+            so.name as item,
+            'Sales Order' as voucher_type,
+            so.name as voucher_no,
+            so.total_qty as qty,
+            so.net_total as rate,
+            so.total as amount,
+            %s as currency,
+            1 as indent  
+        FROM
+            `tabSales Order` AS so
+        WHERE
+            so.project = %s 
+    """, (currency, filters['project']), as_dict=1)
+
+    sales_invoices = frappe.db.sql("""
+        SELECT
+            si.name as item,
+            'Sales Invoice' as voucher_type,
+            si.name as voucher_no,
+            '' as qty,
+            '' as rate,
+            si.total as amount,
+            %s as currency,
+            1 as indent 
+        FROM
+            `tabSales Invoice` AS si
+        WHERE
+            si.project = %s 
+    """, (currency, filters['project']), as_dict=1)
+
+    purchase_invoices = frappe.db.sql("""
+        SELECT
+            pii.item_code as item,
+            'Purchase Invoice' as voucher_type,
+            pi.name as voucher_no,
+            pii.qty as qty,
+            pii.rate as rate,
+            pii.amount as amount,
+            %s as currency,
+            1 as indent 
+        FROM
+            `tabPurchase Invoice` AS pi
+        INNER JOIN
+            `tabPurchase Invoice Item` AS pii ON pi.name = pii.parent
+        WHERE
+            pii.project = %s 
+    """, (currency, filters['project']), as_dict=1)
+
+
+    stock_entries = frappe.db.sql("""
+        SELECT
+            sed.item_code as item,
+            'Stock Entry' as voucher_type,
+            se.name as voucher_no,
+            IFNULL(SUM(sed.qty), 0) as qty,
+            sed.basic_rate as rate,
+            sed.amount as amount,
+            %s as currency,
+            1 as indent 
+        FROM
+            `tabStock Entry` AS se
+        LEFT JOIN
+            `tabStock Entry Detail` AS sed ON se.name = sed.parent
+        WHERE
+            sed.project = %s AND se.purpose = 'Material Issue'
+        GROUP BY
+            sed.item_code
+    """, (currency, filters['project']), as_dict=1)
+
+    timesheets = frappe.db.sql("""
+        SELECT
+            tsd.activity_type as item,
+            'Timesheet' as voucher_type,
+            ts.name as voucher_no,
+            tsd.hours as qty,
+            IFNULL(ts.total_costing_amount / NULLIF(ts.total_hours, 0), 0) as rate,
+            tsd.hours * IFNULL(ts.total_costing_amount / NULLIF(ts.total_hours, 0), 0) as amount,
+            %s as currency,
+            1 as indent 
+        FROM
+            `tabTimesheet` AS ts
+        LEFT JOIN
+            `tabTimesheet Detail` AS tsd ON ts.name = tsd.parent
+        WHERE
+            tsd.project = %s 
+    """, (currency, filters['project']), as_dict=1)
+
+    expense_claims = frappe.db.sql("""
+        SELECT
+            'Expense Claim' as voucher_type,
+            ec.name as voucher_no,
+            '' as qty,
+            '' as rate,
+            ec.total_sanctioned_amount as amount,
+            ec.employee_name as item,
+            %s as currency,
+            1 as indent 
+        FROM
+            `tabExpense Claim` AS ec
+        WHERE
+            ec.project = %s
+    """, (currency, filters['project']), as_dict=1)
+
+    total_amount_orders = sum(so['amount'] for so in sales_orders)
+    total_amount_invoices = sum(si['amount'] for si in sales_invoices)
+    total_amount_purchases = sum(pi['amount'] for pi in purchase_invoices)
+    total_amount_stock_entries = sum(se['amount'] for se in stock_entries)
+    total_amount_timesheets = sum(ts['amount'] for ts in timesheets)
+    total_amount_expense_claims = sum(ec['amount'] for ec in expense_claims)
+    total_cost = sum([total_amount_expense_claims, total_amount_timesheets, total_amount_stock_entries, total_amount_purchases])
+    margin = total_amount_invoices - total_cost
+    margin_ord = total_amount_orders - total_cost
+
+    if total_amount_invoices != 0:
+        margin_per = (margin / total_amount_invoices) * 100
+    else:
+        margin_per = " "
+
+    if total_amount_orders != 0:
+        margin_per_ord = (margin / total_amount_orders ) * 100
+    else:
+        margin_per_ord = " "
+
+    data = []
+    if total_amount_orders:
+        data += [
+            {
+                'item': 'Total Sales Order Amount',
+                'voucher_type': '',
+                'voucher_no': '',
+                'qty': '',
+                'rate': '',
+                'amount': total_amount_orders,
+                'currency': currency,
+                'indent': 0  
+            }
+        ] + sales_orders
+
+    if total_amount_invoices:
+        data += [
+            {
+                'item': 'Total Billed Amount',
+                'voucher_type': '',
+                'voucher_no': '',
+                'qty': '',
+                'rate': '',
+                'amount': total_amount_invoices,
+                'currency': currency,
+                'indent': 0  
+            }
+        ] + sales_invoices
+
+    if total_amount_purchases:
+        data += [
+            {
+                'item': 'Total Purchase Cost',
+                'voucher_type': '',
+                'voucher_no': '',
+                'qty': '',
+                'rate': '',
+                'amount': total_amount_purchases,
+                'currency': currency,
+                'indent': 0  
+            }
+        ] + purchase_invoices
+
+    if total_amount_stock_entries:
+        data += [
+            {
+                'item': 'Total Consumed Material Cost',
+                'voucher_type': '',
+                'voucher_no': '',
+                'qty': '',
+                'rate': '',
+                'amount': total_amount_stock_entries,
+                'currency': currency,
+                'indent': 0  
+            }
+        ] + stock_entries
+
+    if total_amount_timesheets:
+        data += [
+            {
+                'item': 'Total Timesheet Cost',
+                'voucher_type': '',
+                'voucher_no': '',
+                'qty': '',
+                'rate': '',
+                'amount': total_amount_timesheets,
+                'currency': currency,
+                'indent': 0 
+            }
+        ] + timesheets
+
+    if total_amount_expense_claims:
+        data += [
+            {
+                'item': 'Total Expense Claim Cost',
+                'voucher_type': '',
+                'voucher_no': '',
+                'qty': '',
+                'rate': '',
+                'amount': total_amount_expense_claims,
+                'currency': currency,
+                'indent': 0 
+            }
+        ] + expense_claims
+
+
+    data += [
+        {
+            'item': 'Total Cost',
+            'voucher_type': '',
+            'voucher_no': '',
+            'qty': '',
+            'rate': '',
+            'amount': total_cost,
+            'currency': currency,
+            'indent': 0 
+        }
+    ] + [
+        {
+            'item': 'Margin (Based on billing)',
+            'voucher_type': '',
+            'voucher_no': '',
+            'qty': '',
+            'rate': '',
+            'amount': margin,
+            'currency': currency,
+            'indent': 0 
+        }
+    ] + [
+        {
+            'item': 'Margin % (Based on billing)',
+            'voucher_type': '',
+            'voucher_no': '',
+            'qty': '',
+            'rate': '',
+            'amount': margin_per,
+            'currency': currency,
+            'is_percentage': True,
+            'indent': 0 
+        }
+    ] + [
+        {
+            'item': 'Margin (Against order amount)',
+            'voucher_type': '',
+            'voucher_no': '',
+            'qty': '',
+            'rate': '',
+            'amount': margin_ord,
+            'currency': currency,
+            'indent': 0 
+        }
+    ] + [
+        {
+            'item': 'Margin % (Against order amount)',
+            'voucher_type': '',
+            'voucher_no': '',
+            'qty': '',
+            'rate': '',
+            'amount': margin_per_ord,
+            'currency': currency,
+            'is_percentage': True,
+            'indent': 0 
+        }
+    ]
+
+    return data
+
+
